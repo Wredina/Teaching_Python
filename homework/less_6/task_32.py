664747d729725a2f413305b011b992172f3b11af
Sofiya <n.d.dead@mail.ru>
2023-03-31 18:15:04 +0300

    modif homework

--- homework/less_6/task_32.py
+++ homework/less_6/task_32.py
@@ -8,15 +8,12 @@
 from random import randint
 
 size_arr, ran_arr_min, ran_arr_max = randint(9, 15), randint(
-    1, 8), randint(8, 16)  # задаём длину массива и диапазон (cgtwb)
-# выводим всё, что занесли в переменные
+    1, 8), randint(8, 16)
 print(size_arr, ran_arr_min, ran_arr_max)
 
-arr = [randint(1, 16) for _ in range(size_arr)]  # создаём массив
+arr = [randint(1, 16) for _ in range(size_arr)]
 print(arr)
 
-indx_list = []
-for i in range(size_arr):
-    if ran_arr_min - 1 <= arr[i] <= ran_arr_max:
-        indx_list.append(i)
+indx_list = [i for i in range(
+    size_arr) if ran_arr_min - 1 <= arr[i] <= ran_arr_max]
 print(indx_list)
